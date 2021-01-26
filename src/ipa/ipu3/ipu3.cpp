f39638ed2ae9b66402bdd57c74aa0d52a2065620
Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
2021-01-26 10:30:11 +0100

    ipa: ipu3: add support for metering AWB/AE
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>

--- src/ipa/ipu3/ipu3.cpp
+++ src/ipa/ipu3/ipu3.cpp
@@ -52,6 +52,10 @@ private:
 	void fillParams(unsigned int frame, ipu3_uapi_params *params,
 			const ControlList &controls);
 
+	float estimateCCT(float R, float G, float B);
+	void calculateWBGains(Rectangle roi,
+                        const ipu3_uapi_stats_3a *stats);
+
 	void parseStatistics(unsigned int frame,
 			     const ipu3_uapi_stats_3a *stats);
 
@@ -68,6 +72,12 @@ private:
 	uint32_t gain_;
 	uint32_t minGain_;
 	uint32_t maxGain_;
+	uint16_t wbGains_[4];
+	Rectangle metering_regions[15];
+	uint8_t metering_weights[15];
+
+	float brightness_;
+	float colorTemp_;
 };
 
 void IPAIPU3::configure([[maybe_unused]] const CameraSensorInfo &info,
@@ -95,11 +105,53 @@ void IPAIPU3::configure([[maybe_unused]] const CameraSensorInfo &info,
 
 	minExposure_ = std::max(itExp->second.min().get<int32_t>(), 1);
 	maxExposure_ = itExp->second.max().get<int32_t>();
-	exposure_ = maxExposure_;
+	exposure_ = minExposure_;
 
 	minGain_ = std::max(itGain->second.min().get<int32_t>(), 1);
 	maxGain_ = itGain->second.max().get<int32_t>();
-	gain_ = maxGain_;
+	gain_ = minGain_;
+
+	memset(wbGains_, 0, sizeof(wbGains_));
+	brightness_ = 0;
+	colorTemp_ = 0;
+
+	metering_regions[11] = Rectangle(0, 0, 260, 144);
+	metering_regions[9] = Rectangle(260, 0, 1280-2*260, 144);
+	metering_regions[12] = Rectangle(1280-260, 0, 260, 144);
+
+	metering_regions[7] = Rectangle(0, 144, 260, 720-144);
+	metering_regions[13] = Rectangle(0, 720-144, 260, 144);
+
+	metering_regions[8] = Rectangle(1280-260, 144, 260, 720-144);
+	metering_regions[14] = Rectangle(1280-260, 720-144, 260, 144);
+
+	metering_regions[0] = Rectangle(600, 360-144/2, 80, 144);
+	metering_regions[1] = Rectangle(600-80, 360-144/2, 80, 144);
+	metering_regions[2] = Rectangle(600+80, 360-144/2, 80, 144);
+
+	metering_regions[3] = Rectangle(600-80, 360-144/2-144, 3*80, 144);
+	metering_regions[4] = Rectangle(600-80, 360+144/2, 3*80, 144);
+
+	metering_regions[5] = Rectangle(260, 144, 260, 720-144);
+	metering_regions[6] = Rectangle(1280-2*260, 144, 260, 720-144);
+
+	metering_regions[10] = Rectangle(260, 720-144, 1280-2*260, 144);
+
+	metering_weights[0]  = 3;
+	metering_weights[1]  = 3;
+	metering_weights[2]  = 3;
+	metering_weights[3]  = 2;
+	metering_weights[4]  = 2;
+	metering_weights[5]  = 2;
+	metering_weights[6]  = 2;
+	metering_weights[7]  = 1;
+	metering_weights[8]  = 1;
+	metering_weights[9]  = 1;
+	metering_weights[10] = 1;
+	metering_weights[11] = 0;
+	metering_weights[12] = 0;
+	metering_weights[13] = 0;
+	metering_weights[14] = 0;
 
 	setControls(0);
 }
@@ -167,13 +219,54 @@ void IPAIPU3::processEvent(const IPAOperationData &event)
 	}
 }
 
+const struct ipu3_uapi_bnr_static_config imgu_css_bnr_defaults = {
+	{ 16, 16, 16, 16 },			/* wb_gains */
+	{ 255, 255, 255, 255 },			/* wb_gains_thr */
+	{ 0, 0, 8, 6, 0, 14 },			/* thr_coeffs */
+	{ 0, 0, 0, 0 },				/* thr_ctrl_shd */
+	{ -128, 0, -128, 0 },			/* opt_center */
+	{					/* lut */
+		{ 17, 23, 28, 32, 36, 39, 42, 45,
+		  48, 51, 53, 55, 58, 60, 62, 64,
+		  66, 68, 70, 72, 73, 75, 77, 78,
+		  80, 82, 83, 85, 86, 88, 89, 90 }
+	},
+	{ 4, 0, 1, 8, 0, 8, 0, 8, 0 },		/* bp_ctrl */
+	{ 8, 4, 4, 0, 8, 0, 1, 1, 1, 1, 0 },	/* dn_detect_ctrl */
+	1920,
+	{2663424, 1498176},
+};
+
+/* settings for Auto White Balance */
+const struct ipu3_uapi_awb_config_s imgu_css_awb_defaults = {
+	8191, 8191, 8191, 8191|	/* rgbs_thr_gr/r/gb/b */
+	IPU3_UAPI_AWB_RGBS_THR_B_EN | IPU3_UAPI_AWB_RGBS_THR_B_INCL_SAT,
+	.grid = {
+		.width = 160,
+		.height = 45,
+		.block_width_log2 = 3,
+		.block_height_log2 = 4,
+		.x_start = 0,
+		.y_start = 0,
+	},
+};
+
 void IPAIPU3::fillParams(unsigned int frame, ipu3_uapi_params *params,
 			 [[maybe_unused]] const ControlList &controls)
 {
 	/* Prepare parameters buffer. */
 	memset(params, 0, sizeof(*params));
 
-	/* \todo Fill in parameters buffer. */
+	params->use.acc_awb = 1;
+	params->acc_param.awb.config = imgu_css_awb_defaults;
+
+	// Activate wb gain
+	params->use.acc_bnr = 1;
+	params->acc_param.bnr = imgu_css_bnr_defaults;
+	params->acc_param.bnr.wb_gains.gr = wbGains_[0];
+	params->acc_param.bnr.wb_gains.r  = wbGains_[1];
+	params->acc_param.bnr.wb_gains.b  = wbGains_[2];
+	params->acc_param.bnr.wb_gains.gb = wbGains_[3];
 
 	IPAOperationData op;
 	op.operation = IPU3_IPA_ACTION_PARAM_FILLED;
@@ -184,13 +277,69 @@ void IPAIPU3::fillParams(unsigned int frame, ipu3_uapi_params *params,
 	setControls(frame);
 }
 
+float IPAIPU3::estimateCCT(float R, float G, float B)
+{
+	float X=(-0.14282)*(R)+(1.54924)*(G)+(-0.95641)*(B);
+	float Y=(-0.32466)*(R)+(1.57837)*(G)+(-0.73191)*(B);
+	float Z=(-0.68202)*(R)+(0.77073)*(G)+(0.56332)*(B);
+
+	float x=X/(X+Y+Z);
+	float y=Y/(X+Y+Z);
+
+	float n=(x-0.3320)/(0.1858-y);
+	return 449*n*n*n+3525*n*n+6823.3*n+5520.33;
+}
+
+
+void IPAIPU3::calculateWBGains(Rectangle roi, const ipu3_uapi_stats_3a *stats)
+{
+	float Gr=0, R=0, B=0, Gb=0;
+	Point topleft = roi.topLeft();
+	uint32_t startY = (topleft.y / 16) * 160 * 8;
+	uint32_t startX = (topleft.x / 8) * 8;
+	uint32_t endX = startX + (roi.size().width / 8);
+	uint32_t count = 0;
+
+	for (uint32_t j = (topleft.y / 16) ; j < (topleft.y / 16)+(roi.size().height / 16) ; j++) {
+		for (uint32_t i=startX+startY ; i < endX+startY ; i+=8) {
+			Gr += stats->awb_raw_buffer.meta_data[i];
+			R += stats->awb_raw_buffer.meta_data[i+1];
+			B += stats->awb_raw_buffer.meta_data[i+2];
+			Gb += stats->awb_raw_buffer.meta_data[i+3];
+			count++;
+		}
+	}
+	Gr /= count;
+	R  /= count;
+	B  /= count;
+	Gb /= count;
+
+	float G = (Gr+Gb)/2;
+
+	colorTemp_ = estimateCCT(R, G, B);
+	brightness_ = 0.299*R + 0.587*G + 0.114*B;
+
+	wbGains_[0] = 8192*(R/Gr);
+	wbGains_[1] = 8192*(G/R);
+	wbGains_[2] = 8192*(G/B);
+	wbGains_[3] = 8192*(B/Gb);
+}
+
 void IPAIPU3::parseStatistics(unsigned int frame,
 			      [[maybe_unused]] const ipu3_uapi_stats_3a *stats)
 {
 	ControlList ctrls(controls::controls);
+	float brightness = 0;
+	uint32_t sum_weights = 0;
 
-	/* \todo React to statistics and update internal state machine. */
-	/* \todo Add meta-data information to ctrls. */
+	for (uint32_t i = 0; i < 15 ; i++) {
+		calculateWBGains(metering_regions[i], stats);
+		brightness += brightness_ * metering_weights[i];
+		sum_weights += metering_weights[i];
+	}
+	brightness_ = brightness / sum_weights;
+	LOG(IPAIPU3, Error) << "["<<frame<<","<<gain_<<"]"<<"Gain needed: " << 128/brightness_ << " Color temp estimation: " << colorTemp_;
+	calculateWBGains(Rectangle(0, 0, 1280, 720), stats);
 
 	IPAOperationData op;
 	op.operation = IPU3_IPA_ACTION_METADATA_READY;
