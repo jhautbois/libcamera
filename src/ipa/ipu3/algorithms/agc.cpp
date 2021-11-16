jhautbois/libcamera

commit f8f07f9468c6618b73710e3d341ce59ce08ccc08

ipa: ipu3: agc: Improve gain calculation
When an image is partially saturated, its brightness is not increasing
linearly when the shutter time or gain increases. It is a big issue with
a backlight as the algorithm is fading to darkness right now.

Introduce a function to estimate the brightness of the frame, based on
the current exposure/gain and loop on it several times to estimate it
again and approach the non linear function.

Inspired-by: 7de5506c30b3 ("libcamera: src: ipa: raspberrypi: agc: Improve gain update calculation for partly saturated images")
Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Umang Jain <umang.jain@ideasonboard.com>

src/ipa/ipu3/algorithms/agc.cpp
@@ -67,6 +67,17 @@ static constexpr uint32_t kMinCellsPerZoneRatio = 255 * 20 / 100;
 /* Number of frames to wait before calculating stats on minimum exposure */
 static constexpr uint32_t kNumStartupFrames = 10;
 
+/* Maximum luminance used for brightness normalization */
+static constexpr uint32_t kMaxLuminance = 255;
+
+/*
+ * Normalized luma value target.
+ *
+ * It's a number that's chosen so that, when the camera points at a grey
+ * target, the resulting image brightness is considered right.
+ */
+static constexpr double kNormalizedLumaTarget = 0.16;
+
 Agc::Agc()
 	: frameCount_(0), iqMean_(0.0), lineDuration_(0s), minExposureLines_(0),
 	  maxExposureLines_(0), filteredExposure_(0s), currentExposure_(0s),
@@ -185,23 +196,32 @@ void Agc::filterExposure()
 /**
  * \brief Estimate the new exposure and gain values
  * \param[inout] frameContext The shared IPA frame Context
+ * \param[in] currentYGain The gain calculated on the current brightness level
  */
-void Agc::computeExposure(IPAFrameContext &frameContext)
+void Agc::computeExposure(IPAFrameContext &frameContext, double currentYGain)
 {
 	/* Get the effective exposure and gain applied on the sensor. */
 	uint32_t exposure = frameContext.sensor.exposure;
 	double analogueGain = frameContext.sensor.gain;
 
-	/* Estimate the gain needed to have the proportion wanted */
+	/*
+	 * Estimate the gain needed to have the proportion of pixels in a given
+	 * desired range. iqMean_ returns the mean value of the top 2% of the
+	 * cumulative histogram, and we want it to be as close as possible to a
+	 * configured target.
+	 */
 	double evGain = kEvGainTarget * knumHistogramBins / iqMean_;
 
-	if (std::abs(evGain - 1.0) < 0.01) {
+	if (evGain < currentYGain)
+		evGain = currentYGain;
+
+	/* Consider within 1% of the target as correctly exposed */
+	if (std::abs(evGain - 1.0) < 0.01)
 		LOG(IPU3Agc, Debug) << "We are well exposed (iqMean = "
 				    << iqMean_ << ")";
-		return;
-	}
 
 	/* extracted from Rpi::Agc::computeTargetExposure */
+
 	/* Calculate the shutter time in seconds */
 	utils::Duration currentShutter = exposure * lineDuration_;
 	LOG(IPU3Agc, Debug) << "Actual total exposure " << currentShutter * analogueGain
@@ -258,6 +278,56 @@ void Agc::computeExposure(IPAFrameContext &frameContext)
 }
 
 /**
+ * \brief Estimate the average brightness of the frame
+ * \param[in] frameContext The shared IPA frame context
+ * \param[in] grid The grid used to store the statistics in the IPU3
+ * \param[in] stats The IPU3 statistics and ISP results
+ * \param[in] currentYGain The gain calculated on the current brightness level
+ * \return The normalized luma
+ *
+ * Luma is the weighted sum of gamma-compressed R′G′B′ components of a color
+ * video. The luma values are normalized as 0.0 to 1.0, with 1.0 being a
+ * theoretical perfect reflector of 100% reference white. We use the Rec. 601
+ * luma here.
+ *
+ * More detailed information can be found in:
+ * https://en.wikipedia.org/wiki/Luma_(video)
+ */
+double Agc::computeInitialY(IPAFrameContext &frameContext,
+			    const ipu3_uapi_grid_config &grid,
+			    const ipu3_uapi_stats_3a *stats,
+			    double currentYGain)
+{
+	double redSum = 0, greenSum = 0, blueSum = 0;
+
+	for (unsigned int cellY = 0; cellY < grid.height; cellY++) {
+		for (unsigned int cellX = 0; cellX < grid.width; cellX++) {
+			uint32_t cellPosition = cellY * stride_ + cellX;
+
+			const ipu3_uapi_awb_set_item *cell =
+				reinterpret_cast<const ipu3_uapi_awb_set_item *>(
+					&stats->awb_raw_buffer.meta_data[cellPosition]
+				);
+
+			redSum += cell->R_avg * currentYGain;
+			greenSum += (cell->Gr_avg + cell->Gb_avg) / 2 * currentYGain;
+			blueSum += cell->B_avg * currentYGain;
+		}
+	}
+
+	/*
+	 * Estimate the sum of the brightness values, weighted with the gains
+	 * applied on the channels in AWB as the Rec. 601 luma.
+	 */
+	double Y_sum = redSum * frameContext.awb.gains.red * .299 +
+		       greenSum * frameContext.awb.gains.green * .587 +
+		       blueSum * frameContext.awb.gains.blue * .114;
+
+	/* Return the normalized relative luminance. */
+	return Y_sum / (grid.height * grid.width) / kMaxLuminance;
+}
+
+/**
  * \brief Process IPU3 statistics, and run AGC operations
  * \param[in] context The shared IPA context
  * \param[in] stats The IPU3 statistics and ISP results
@@ -268,7 +338,29 @@ void Agc::computeExposure(IPAFrameContext &frameContext)
 void Agc::process(IPAContext &context, const ipu3_uapi_stats_3a *stats)
 {
 	measureBrightness(stats, context.configuration.grid.bdsGrid);
-	computeExposure(context.frameContext);
+
+	double currentYGain = 1.0;
+	double targetY = kNormalizedLumaTarget;
+
+	/*
+	 * Do this calculation a few times as brightness increase can be
+	 * non-linear when there are saturated regions.
+	 */
+	for (int i = 0; i < 8; i++) {
+		double initialY = computeInitialY(context.frameContext,
+						  context.configuration.grid.bdsGrid,
+						  stats, currentYGain);
+		double extra_gain = std::min(10.0, targetY / (initialY + .001));
+
+		currentYGain *= extra_gain;
+		LOG(IPU3Agc, Debug) << "Initial Y " << initialY
+				    << " target " << targetY
+				    << " gives gain " << currentYGain;
+		if (extra_gain < 1.01)
+			break;
+	}
+
+	computeExposure(context.frameContext, currentYGain);
 	frameCount_++;
 }
 

src/ipa/ipu3/algorithms/agc.h
@@ -34,7 +34,11 @@ private:
 	void measureBrightness(const ipu3_uapi_stats_3a *stats,
 			       const ipu3_uapi_grid_config &grid);
 	void filterExposure();
-	void computeExposure(IPAFrameContext &frameContext);
+	void computeExposure(IPAFrameContext &frameContext, double currentYGain);
+	double computeInitialY(IPAFrameContext &frameContext,
+			       const ipu3_uapi_grid_config &grid,
+			       const ipu3_uapi_stats_3a *stats,
+			       double currentYGain);
 
 	uint64_t frameCount_;
 

commit 5f9d8268722976d70aa36960c83769543804f83f

ipa: ipu3: agc: Saturate the averages when computing relative luminance
The relative luminance is calculated using an iterative process to
account for saturation in the sensor, as multiplying pixels by a gain
doesn't increase the relative luminance by the same factor if some
regions are saturated. Relative luminance estimation doesn't apply a
saturation, which produces a value that doesn't match what the sensor
will output, and defeats the point of the iterative process. Fix it.

Fixes: f8f07f9468c6 ("ipa: ipu3: agc: Improve gain calculation")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/ipa/ipu3/algorithms/agc.cpp
@@ -252,10 +252,19 @@ void Agc::computeExposure(IPAFrameContext &frameContext, double yGain,
  * \param[in] gain The analogue gain to apply to the frame
  * \return The relative luminance
  *
- * Luma is the weighted sum of gamma-compressed R′G′B′ components of a color
- * video. The luma values are normalized as 0.0 to 1.0, with 1.0 being a
- * theoretical perfect reflector of 100% reference white. We use the Rec. 601
- * luma here.
+ * This function estimates the average relative luminance of the frame that
+ * would be output by the sensor if an additional analogue \a gain was applied.
+ *
+ * The estimation is based on the AWB statistics for the current frame. Red,
+ * green and blue averages for all cells are first multiplied by the gain, and
+ * then saturated to approximate the sensor behaviour at high brightness
+ * values. The approximation is quitte rough, as it doesn't take into account
+ * non-linearities when approaching saturation.
+ *
+ * The relative luminance (Y) is computed from the linear RGB components using
+ * the Rec. 601 formula. The values is normalized to the [0.0, 1.0] range,
+ * where 1.0 corresponds to a theoretical perfect reflector of 100% reference
+ * white.
  *
  * More detailed information can be found in:
  * https://en.wikipedia.org/wiki/Relative_luminance
@@ -267,6 +276,7 @@ double Agc::estimateLuminance(IPAFrameContext &frameContext,
 {
 	double redSum = 0, greenSum = 0, blueSum = 0;
 
+	/* Sum the per-channel averages, saturated to 255. */
 	for (unsigned int cellY = 0; cellY < grid.height; cellY++) {
 		for (unsigned int cellX = 0; cellX < grid.width; cellX++) {
 			uint32_t cellPosition = cellY * stride_ + cellX;
@@ -275,10 +285,11 @@ double Agc::estimateLuminance(IPAFrameContext &frameContext,
 				reinterpret_cast<const ipu3_uapi_awb_set_item *>(
 					&stats->awb_raw_buffer.meta_data[cellPosition]
 				);
+			const uint8_t G_avg = (cell->Gr_avg + cell->Gb_avg) / 2;
 
-			redSum += cell->R_avg * gain;
-			greenSum += (cell->Gr_avg + cell->Gb_avg) / 2 * gain;
-			blueSum += cell->B_avg * gain;
+			redSum += std::min(cell->R_avg * gain, 255.0);
+			greenSum += std::min(G_avg * gain, 255.0);
+			blueSum += std::min(cell->B_avg * gain, 255.0);
 		}
 	}
 