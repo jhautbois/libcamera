8040fbea47cfcfd3315baf80862787d0ec8af65a
Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>
2021-11-18 14:43:24 +0100

    ipa: rkisp1: Introduce IPAContext
    Before using any algorithm, we want the IPAContext to be ready for
    those. Introduce the IPAContext as in IPA::IPU3 but empty. Each
    algorithm will then introduce the needed fields.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@ideasonboard.com>

---
+++ src/ipa/rkisp1/ipa_context.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2021, Google Inc.
+ *
+ * ipa_context.h - RkISP1 IPA Context
+ *
+ */
+#ifndef __LIBCAMERA_RKISP1_IPA_CONTEXT_H__
+#define __LIBCAMERA_RKISP1_IPA_CONTEXT_H__
+
+#include <linux/rkisp1-config.h>
+
+#include <libcamera/base/utils.h>
+
+#include <libcamera/geometry.h>
+
+namespace libcamera {
+
+namespace ipa::rkisp1 {
+
+struct IPASessionConfiguration {
+};
+
+struct IPAFrameContext {
+};
+
+struct IPAContext {
+	IPASessionConfiguration configuration;
+	IPAFrameContext frameContext;
+};
+
+} /* namespace ipa::rkisp1 */
+
+} /* namespace libcamera*/
+
+#endif /* __LIBCAMERA_RKISP1_IPA_CONTEXT_H__ */
